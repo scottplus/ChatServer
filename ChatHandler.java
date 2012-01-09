f2c469abd8246b967b0fb6c88173d8089a572fc8
bpsulliv <bradpsulliv@gmail.com>
2012-01-09 14:17:16 -0500

    Update ChatHandler.java

--- ChatHandler.java
+++ ChatHandler.java
@@ -1,1 +1,163 @@
-yoyo
+//package chatserver;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class ChatHandler extends Thread {
+    
+    private Socket connectionToClient;
+    private DataInputStream i;
+    private DataOutputStream o;
+    private boolean running = false;
+    private static Vector handlers = new Vector();
+    
+    //commands available to the API
+    private String commands[] = {"./quit","./username", "./help"};
+    
+    //index
+    private int index;
+    
+    //username
+    private String username;
+    
+    //running program
+
+    public ChatHandler(Socket connection) throws IOException {
+        //handshake to the server, setup data streams
+        this.connectionToClient = connection;
+                
+        //byte streams
+        i = new DataInputStream(new BufferedInputStream(connectionToClient.getInputStream()));
+        o = new DataOutputStream(new BufferedOutputStream(connectionToClient.getOutputStream()));
+    }
+
+    public void run() { 
+        //index is the size of the array currently
+        index = handlers.size();
+        
+        //add the current instance to the static vector
+        handlers.insertElementAt(this, this.index);
+        
+        //set the anonymous username
+        username = "guest";
+        
+        //set our boolean
+        running = true;
+        
+        //listen for incomming messages
+        listen();
+    }
+    
+    //listen for incomming messages
+    private synchronized void listen() {
+        try {
+            while(running) {
+                String msg = i.readUTF(); //blocking IO call
+                
+                if(msg.substring(0,2).equals("./")) {
+                    //if message is a command, call the api
+                    control(msg);                    
+                } else {
+                    //else send the message
+                    broadcast(username+": "+msg);                    
+                }
+            }
+        } catch (IOException e){
+            //catch the IOException to handle the dropped byte stream when a user disconnects
+            broadcast("Server the user "+username+" has disconnected from the server");
+            System.out.println("The user: "+username+" has disconnected from the server");     
+        }
+    }
+    
+    //control method for user-server interaction
+    public void control(String month) {
+        //need to change to switch statement, JDK 7 required ***************************************************************************
+        
+        ChatHandler current = (ChatHandler) handlers.get(index);
+        
+        try {
+            //if the user types './quit'
+            if(month.toLowerCase().contains("./quit")) {
+                //quit the server
+                
+                //let the users know they have disconnected
+                broadcast("Server: The user "+username+" has disconnected from the server");
+                System.out.println("The user: "+username+" has disconnected from the server");
+                
+                //stop listening for messages
+                running = false;
+                
+                //close the bytestreams
+                current.i.close();
+                current.o.close();
+                
+                //remove it from the vector
+                handlers.remove(index);
+                
+            } else if(month.toLowerCase().contains("./username")) {
+                //change the username
+                String tempUsername = username;
+                
+                this.username = month.substring("./username ".length(),month.length());
+                
+                //broadcast to all users
+                broadcast("Server: "+tempUsername + " has changed their username to "+username);
+                System.out.println("Server: "+tempUsername + " has changed their username to "+username);
+            } else if(month.toLowerCase().contains("./help")) {
+                //display commands available to the user
+                
+                //display string
+                String output = "Server: Commands available are \n ";
+                
+                //append the strings
+                for(String append: commands) {
+                    output += append + "\n";
+                }
+                
+                //broadcast to the user
+                ChatHandler.broadcast(output);
+                
+            }else {
+                //if no API call available broadcast to the calling index
+                ChatHandler.broadcast("Server: No API call under that name, please try again", index);
+                System.out.println(username + " attempted an illegal API call");
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error with the API: " + e);
+        }
+    }
+
+    //send to all the instances of NetworkObject
+   private static void broadcast(String message) {
+       //synchronize the vector
+        synchronized (handlers) {
+            Enumeration e = handlers.elements();
+            while (e.hasMoreElements()) {
+                ChatHandler c = (ChatHandler) e.nextElement();
+                try {
+                    c.o.writeUTF(message);
+                    c.o.flush();
+                } catch (IOException ex) {
+                    c.stop();
+                }
+            }
+        }
+    }
+    
+    //send to specific instance of NetworkObject
+    private static void broadcast(String message, int index) {
+        //static method synchronize the NetworkObject
+        ChatHandler c = (ChatHandler) handlers.get(index);
+        synchronized (c) {
+            //write 
+            try {
+                c.o.writeUTF(message);
+                c.o.flush();
+            } catch (IOException ex) {
+                   //control("./quit");
+            }
+         }
+    }
+}
